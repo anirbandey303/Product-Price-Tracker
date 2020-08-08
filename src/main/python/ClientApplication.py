972c56eb1c040bf3b935bcf0af07b7949e5fbbef
Anirban Dey <anirbandey303@gmail.com>
2020-08-08 12:29:34 +0530

    Added to-do for client application file

--- src/main/python/ClientApplication.py
+++ src/main/python/ClientApplication.py
@@ -1,3 +1,19 @@
 #Import TrackerFactory
 #Create Object of TrackerFactory
 #Call check() function with necessary parameters
+
+#an option to decide if to look for a product
+# or use the priviously stored URLs and check their price
+
+#takes input from user for URL
+#takes input from user for Expected Price
+#takes input from user for website to look for
+
+#imports the factory class inside TrackerFactory.py
+#creates an object of the factory class
+#calles the check() function with the parameters
+#display the output
+
+#an option to priodically check for the same.
+
+#an option to store the URL, price and website in a file (Y/N).
